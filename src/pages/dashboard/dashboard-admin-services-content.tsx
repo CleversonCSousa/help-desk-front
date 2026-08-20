53b04e57f3041ce9af6753a51a811e55738d79b0
CleversonCSousa <cleversonsousa.git@gmail.com>
2026-08-20 13:35:40 -0300

    feat(service): update layout to render the services

--- src/pages/dashboard/dashboard-admin-services-content.tsx
+++ src/pages/dashboard/dashboard-admin-services-content.tsx
@@ -1,7 +1,14 @@
 import { Ban, PenLineIcon } from "lucide-react";
 import { CreateServiceModal } from "./create-service-modal";
+import { useListServicesQuery } from "../../features/service/api-slice";
 
 export const DashboardAdminServicesContent = () => {
+  const { data: services, isLoading } = useListServicesQuery();
+  console.log("Serviços retornados da API:", services);
+  if (isLoading) {
+    return null;
+  }
+
   return (
     <main className="flex-1 overflow-y-auto rounded-tl-3xl bg-white p-4 text-gray-200 min-[381px]:p-6 md:mt-3 md:p-12 md:pt-13 md:pr-12 md:pb-12">
       <div className="flex items-center justify-between">
@@ -26,33 +33,43 @@ export const DashboardAdminServicesContent = () => {
           </thead>
 
           <tbody className="divide-y divide-gray-500">
-            <tr className="transition-colors hover:bg-gray-50">
-              <td className="px-1.5 py-1 align-middle font-bold text-gray-900 min-[381px]:px-3 min-[381px]:py-2 md:px-6 md:py-4">
-                <span className="min-[381px]:hidden">Instalaç...</span>
-                <span className="hidden min-[381px]:inline">
-                  Network installation
-                </span>
-              </td>
-              <td className="px-1.5 py-1 align-middle whitespace-nowrap text-gray-700 min-[381px]:px-3 min-[381px]:py-2 md:px-6 md:py-4">
-                R$ 180,00
-              </td>
-              <td className="px-1.5 py-1 align-middle whitespace-nowrap min-[381px]:px-3 min-[381px]:py-2 md:px-6 md:py-4">
-                <div className="text-feedback-done bg-feedback-done/20 mx-auto flex w-min items-center gap-1.5 rounded-full px-3 py-1 text-sm font-medium">
-                  <span>Active</span>
-                </div>
-              </td>
-              <td className="px-1.5 py-1 align-middle whitespace-nowrap min-[381px]:px-3 min-[381px]:py-2 md:px-6 md:py-4">
-                <div className="flex items-center gap-3">
-                  <button className="flex items-center gap-1 text-sm text-gray-300 hover:text-gray-700">
-                    <Ban size={16} />
-                    <span className="hidden md:inline">Deactivate</span>
-                  </button>
-                  <button className="flex h-8 w-8 items-center justify-center rounded-md bg-gray-500 transition-colors">
-                    <PenLineIcon size={16} />
-                  </button>
-                </div>
-              </td>
-            </tr>
+            {services && services.length > 0 ? (
+              services.map((service) => (
+                <tr className="transition-colors hover:bg-gray-50">
+                  <td className="px-1.5 py-1 align-middle font-bold text-gray-900 min-[381px]:px-3 min-[381px]:py-2 md:px-6 md:py-4">
+                    <span className="min-[381px]:hidden">Instalaç...</span>
+                    <span className="hidden min-[381px]:inline">
+                      {service.title}
+                    </span>
+                  </td>
+                  <td className="px-1.5 py-1 align-middle whitespace-nowrap text-gray-700 min-[381px]:px-3 min-[381px]:py-2 md:px-6 md:py-4">
+                    R$ {service.price.toFixed(2)}
+                  </td>
+                  <td className="px-1.5 py-1 align-middle whitespace-nowrap min-[381px]:px-3 min-[381px]:py-2 md:px-6 md:py-4">
+                    <div className="text-feedback-done bg-feedback-done/20 mx-auto flex w-min items-center gap-1.5 rounded-full px-3 py-1 text-sm font-medium">
+                      <span>{service.isActive ? "Active" : "Inactive"}</span>
+                    </div>
+                  </td>
+                  <td className="px-1.5 py-1 align-middle whitespace-nowrap min-[381px]:px-3 min-[381px]:py-2 md:px-6 md:py-4">
+                    <div className="flex items-center gap-3">
+                      <button className="flex items-center gap-1 text-sm text-gray-300 hover:text-gray-700">
+                        <Ban size={16} />
+                        <span className="hidden md:inline">Deactivate</span>
+                      </button>
+                      <button className="flex h-8 w-8 items-center justify-center rounded-md bg-gray-500 transition-colors">
+                        <PenLineIcon size={16} />
+                      </button>
+                    </div>
+                  </td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td colSpan={4} className="py-8 text-center text-gray-400">
+                  No services
+                </td>
+              </tr>
+            )}
           </tbody>
         </table>
       </div>
