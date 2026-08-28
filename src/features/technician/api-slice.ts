deeb987bc9c347e4afcf2d56adc41df248fe3f66
CleversonCSousa <cleversonsousa.git@gmail.com>
2026-08-28 16:31:55 -0300

    feat(technician): create a mutation to update technician

--- src/features/technician/api-slice.ts
+++ src/features/technician/api-slice.ts
@@ -1,7 +1,7 @@
 import { apiSlice } from "../../api/api-slice";
 
 type WorkingHour = {
-  id: string;
+  id?: string;
   timeSlot: string;
 };
 
@@ -13,6 +13,17 @@ type Technician = {
   workingHours: Array<WorkingHour>;
 };
 
+type UpdateTechnicianResponse = {
+  message: string;
+};
+
+type UpdateTechnicianRequest = {
+  id: string;
+  name: string;
+  email: string;
+  workingHours: Array<WorkingHour>;
+};
+
 export const technicianApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     listTechnicians: builder.query<Array<Technician>, void>({
@@ -21,7 +32,40 @@ export const technicianApiSlice = apiSlice.injectEndpoints({
         method: "GET",
       }),
     }),
+    updateTechnician: builder.mutation<
+      UpdateTechnicianResponse,
+      UpdateTechnicianRequest
+    >({
+      query: (technician) => ({
+        url: `/technicians`,
+        method: "PUT",
+        body: technician,
+      }),
+      async onQueryStarted({ id, ...put }, { dispatch, queryFulfilled }) {
+        try {
+          await queryFulfilled;
+          dispatch(
+            technicianApiSlice.util.updateQueryData(
+              "listTechnicians",
+              undefined,
+              (draft) => {
+                const technician = draft.find((t) => t.id === id);
+
+                if (technician) {
+                  technician.name = put.name;
+                  technician.email = put.email;
+                  technician.workingHours = put.workingHours;
+                }
+              },
+            ),
+          );
+        } catch (error) {
+          console.error("Failed to update technician cache", error);
+        }
+      },
+    }),
   }),
 });
 
-export const { useListTechniciansQuery } = technicianApiSlice;
+export const { useListTechniciansQuery, useUpdateTechnicianMutation } =
+  technicianApiSlice;
